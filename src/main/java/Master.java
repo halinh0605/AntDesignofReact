eba3dfff0a26a77304d9eb075f6e0cbd9bf620d6
halinh0605 <linhha0605@gmail.com>
2020-02-10 21:52:16 +0700

    remove

--- src/main/java/Master.java
+++ src/main/java/Master.java
@@ -1,27 +1,92 @@
+import org.apache.commons.io.FileUtils;
 import org.junit.BeforeClass;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Master {
     static WebDriver webDriver;
+    private String PATH_RESULT = "TEST_RESULT";
 
     @BeforeClass
-    public static void  setUpClass() {
+    public static void setUpClass() {
         System.out.println("Master setup");
-        System.setProperty("webdriver.chrome.driver", "D:\\setup\\chromedriver.exe");
-        webDriver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setHeadless(true);
+        webDriver = new ChromeDriver(chromeOptions);
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
-    protected String randomCharacter(int length){
+
+    @Rule
+    public TestRule watcher = new TestWatcher() {
+        @Override
+        protected void succeeded(Description description) {
+            System.out.println("Pass!");
+            webDriver.close();
+        }
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            System.out.println("Fail!");
+            takeScreenShot(description);
+            webDriver.close();
+        }
+    };
+
+    private void takeScreenShot(Description desc) {
+        String methodName = desc.getMethodName();
+
+        try {
+            String pathOut = PATH_RESULT + File.separator + desc.getTestClass().getSimpleName();
+            File destinyFolder = new File(pathOut);
+            if (!destinyFolder.exists()) {
+                destinyFolder.mkdir();
+            }
+            File screenshotFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+            Date now = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HHmmss");
+            FileUtils.copyFile(screenshotFile, new File(pathOut + File.separator + methodName + sdf.format(now) + ".png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void takeScreenShot() {
+        try {
+            String pathOut = PATH_RESULT + File.separator + "DEBUG";
+            File destinyFolder = new File(pathOut);
+            if (!destinyFolder.exists()) {
+                destinyFolder.mkdir();
+            }
+            File screenshotFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+            Date now = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HHmmss");
+            FileUtils.copyFile(screenshotFile, new File(pathOut + File.separator +
+                    sdf.format(now.getTime()) + ".png"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected String randomCharacter(int length) {
         String result = "";
         Random r = new Random();
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
@@ -30,20 +95,22 @@ public class Master {
         }
         return result;
     }
-    void selectList (String xpath, String value){
+
+    void selectList(String xpath, String value) {
         webDriver.findElement(By.xpath(xpath)).click();
         String optionSelect = String.format("//select[@id='select-demo']//option[text()='%s']", value);
         WebElement optionXpath = webDriver.findElement(By.xpath(optionSelect));
         waitForElement(optionSelect);
         optionXpath.click();
     }
-    protected String getRandomFromList(String list){
+
+    protected String getRandomFromList(String list) {
         String temp[] = list.split(";");
         Random ran = new Random();
         return temp[ran.nextInt(temp.length)];
     }
 
-    void selecMultitList (String xpath, String value){
+    void selecMultitList(String xpath, String value) {
         webDriver.findElement(By.xpath(xpath)).click();
         String optionSelect = String.format("//select[@id='multi-select']//option[text()='%s']", value);
         WebElement optionXpath = webDriver.findElement(By.xpath(optionSelect));
@@ -51,7 +118,7 @@ public class Master {
         optionXpath.click();
     }
 
-    void DropDown (String xpath, String value){
+    void DropDown(String xpath, String value) {
         webDriver.findElement(By.xpath(xpath)).click();
         String optionSelect = String.format("//select[@id='select2-country-container']//option[text()='%s']", value);
         WebElement optionXpath = webDriver.findElement(By.xpath(optionSelect));
@@ -67,77 +134,76 @@ public class Master {
 
     void selectDropdown(String xpath, String value) {
         webDriver.findElement(By.xpath(xpath)).click();
-        String optionStr =String.format("//div[contains(@class,'ant-select-dropdown')]//ul[@role='listbox']//li[text()='%s']", value) ;
+        String optionStr = String.format("//div[contains(@class,'ant-select-dropdown')]//ul[@role='listbox']//li[text()='%s']", value);
         WebElement optionXpath = webDriver.findElement(
                 By.xpath(optionStr));
         waitForElement(optionStr);
         optionXpath.click();
     }
 
-    void transfer(String xpath, String value){
-        String optionTrans = String.format("//section[@id='components-transfer-demo-basic']//span[text()='%s']",value);
+    void transfer(String xpath, String value) {
+        String optionTrans = String.format("//section[@id='components-transfer-demo-basic']//span[text()='%s']", value);
         WebElement optionXpath = webDriver.findElement(By.xpath(optionTrans));
         optionXpath.click();
     }
 
     /**
      * @param xpath
-     * @param value  format like this:"value1;value2"
+     * @param value         format like this:"value1;value2"
      * @param isRightToLeft True is click Right to left
      */
-    protected void selectTransfer(String xpath, String value, boolean isRightToLeft){
+    protected void selectTransfer(String xpath, String value, boolean isRightToLeft) {
         String[] temp = value.split(";");
-        for(String s:temp){
-            String optionTrans = String.format(xpath+"//span[text()='%s']",s);
+        for (String s : temp) {
+            String optionTrans = String.format(xpath + "//span[text()='%s']", s);
             clickElement(optionTrans);
         }
 
-        if(isRightToLeft){
-            waitForElement(xpath+"//i[contains(@class,'anticon-right')]");
-            clickElementUseJS(xpath+"//i[contains(@class,'anticon-right')]");
-        }else {
-            waitForElement(xpath+"//i[contains(@class,'anticon-left')]");
-            clickElementUseJS(xpath+"//i[contains(@class,'anticon-left')]");
+        if (isRightToLeft) {
+            waitForElement(xpath + "//i[contains(@class,'anticon-right')]");
+            clickElementUseJS(xpath + "//i[contains(@class,'anticon-right')]");
+        } else {
+            waitForElement(xpath + "//i[contains(@class,'anticon-left')]");
+            clickElementUseJS(xpath + "//i[contains(@class,'anticon-left')]");
         }
     }
 
-    protected void clickElement(String xpath){
+    protected void clickElement(String xpath) {
         WebElement myElement = webDriver.findElement(By.xpath(xpath));
         myElement.click();
     }
 
-    protected void clickElementUseJS(String xpath){
+    protected void clickElementUseJS(String xpath) {
         WebElement myelement = webDriver.findElement(By.xpath(xpath));
 
         ((JavascriptExecutor) webDriver).executeScript("arguments[0].click();", myelement);
     }
 
-    protected void clickElementUseJSByid(String id){
+    protected void clickElementUseJSByid(String id) {
         WebElement myelement = webDriver.findElement(By.id(id));
 
         ((JavascriptExecutor) webDriver).executeScript("arguments[0].click();", myelement);
     }
 
-    protected void scrollToElement(String xpath){
+    protected void scrollToElement(String xpath) {
         WebElement myelement = webDriver.findElement(By.xpath(xpath));
         ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView();", myelement);
     }
 
 
-    protected void inputTextToElement(String xpath, String value){
+    protected void inputTextToElement(String xpath, String value) {
         WebElement optionXpath = webDriver.findElement(By.xpath(xpath));
         optionXpath.sendKeys(value);
     }
 
     void selectListBox(String xpath, String value) {
         webDriver.findElement(By.xpath(xpath)).click();
-        String optionStr =String.format("//div[@class='dual-list list-left col-md-5']//ul[@class='list-group']//li[contains(.,'%s')]", value) ;
+        String optionStr = String.format("//div[@class='dual-list list-left col-md-5']//ul[@class='list-group']//li[contains(.,'%s')]", value);
         WebElement optionXpath = webDriver.findElement(
                 By.xpath(optionStr));
         waitForElement(optionStr);
         optionXpath.click();
     }
-
 
 
 }
--- src/main/java/TestComponent.java
+++ src/main/java/TestComponent.java
@@ -1,61 +1,61 @@
 import org.junit.Test;
-        import org.openqa.selenium.By;
-        import org.openqa.selenium.JavascriptExecutor;
-        import org.openqa.selenium.WebElement;
-        import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
-        import javax.swing.event.TreeSelectionEvent;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+public class TestComponent extends Master {
 
-public class TestComponent extends Master{
-
-//ant - Select
+    //ant - Select
     @Test
-    public void testSelect(){
+    public void testSelect() {
         webDriver.get("https://ant.design/components/select/");
-        String xx= getRandomFromList("Lucy;Jack;Tom");
-        selectDropdown("//*[@id='components-select-demo-search']//div",xx);
+        String xx = getRandomFromList("Lucy;Jack;Tom");
+        selectDropdown("//*[@id='components-select-demo-search']//div", xx);
+//        takeScreenShot();
         String result = webDriver.findElement(By.xpath("//*[@id='components-select-demo-search']//div[@class='ant-select-selection-selected-value']")).getText();
-        assertEquals(result,xx);
+
+        assertEquals(result, "hfgf");
     }
 
-//ant - TreeSelect
+    //ant - TreeSelect
     @Test
-    public void testTreeSelect(){
+    public void testTreeSelect() {
         webDriver.get("https://ant.design/components/tree-select/");
         webDriver.findElement(By.xpath("//section[@id='components-tree-select-demo-basic']//section[@class='code-box-demo']//span[@class='ant-select-selection__placeholder']")).click();
         webDriver.findElement(By.xpath("//span[@class='ant-select-tree-title'][contains(.,'my leaf')]")).click();
         String result = webDriver.findElement(By.xpath("//span[@class='ant-select-selection__rendered'][contains(.,'my leaf')]")).getText();
-        assertEquals(result,"my leaf");
+        assertEquals(result, "my leaf");
     }
 
-//ant- Button
+    //ant- Button
     @Test
-    public void testButton(){
+    public void testButton() {
         webDriver.get("https://ant.design/components/button/");
         webDriver.findElement(By.xpath("//*[@id='components-button-demo-size']//span[contains(.,'Small')]")).click();
     }
 
-//ant - Cascader
+    //ant - Cascader
     @Test
-    public void testCascader(){
+    public void testCascader() {
         webDriver.get("https://ant.design/components/cascader/");
         webDriver.findElement(By.xpath("//*[@id='components-cascader-demo-basic']//span[@class='ant-cascader-picker-label']")).click();
     }
 
-//ant - Datepicker
+    //ant - Datepicker
     @Test
-    public void testDatepicker(){
+    public void testDatepicker() {
         webDriver.get("https://ant.design/components/date-picker/");
         WebElement inputDate = webDriver.findElement(By.xpath("//*[@id='components-date-picker-demo-basic']//span[@class='ant-calendar-picker']//input[@placeholder='Select date']"));
         inputDate.click();
         webDriver.findElement(By.xpath("//input[@placeholder='Select date' and contains(@class, 'ant-calendar-input') ]")).sendKeys("2020-01-14");
     }
 
-//ant - Form
+    //ant - Form
     @Test
-    public void testForm1(){
+    public void testForm1() {
         webDriver.get("https://ant.design/components/form/");
         webDriver.findElement(By.id("horizontal_login_username")).sendKeys("user");
         webDriver.findElement(By.id("horizontal_login_password")).sendKeys("12345");
@@ -63,7 +63,7 @@ public class TestComponent extends Master{
     }
 
     @Test
-    public void testForm2(){
+    public void testForm2() {
         webDriver.get("https://ant.design/components/form/");
         WebElement Element = webDriver.findElement(By.id("normal_login_username"));
         JavascriptExecutor js = (JavascriptExecutor) webDriver;
@@ -75,7 +75,7 @@ public class TestComponent extends Master{
     }
 
     @Test
-    public void testForm3(){
+    public void testForm3() {
         webDriver.get("https://ant.design/components/form/");
         WebElement Element = webDriver.findElement(By.id("register_email"));
         JavascriptExecutor js = (JavascriptExecutor) webDriver;
@@ -87,12 +87,13 @@ public class TestComponent extends Master{
         webDriver.findElement(By.id("register_nickname")).sendKeys("banhbao");
 
         clickElementUseJS("//*[@id='register_residence']");
-            waitForElement("//li[contains(@title,'Zhejiang')]");
+        waitForElement("//li[contains(@title,'Zhejiang')]");
         clickElementUseJS("//li[contains(@title,'Zhejiang')]");
         waitForElement("//li[contains(@title,'Hangzhou')]");
         clickElementUseJS("//li[contains(@title,'Hangzhou')]");
         waitForElement("//li[contains(@title,'West Lake')]");
-        clickElementUseJS("//li[contains(@title,'West Lake')]");;
+        clickElementUseJS("//li[contains(@title,'West Lake')]");
+        ;
 
         webDriver.findElement(By.xpath("//input[@id='register_phone']")).sendKeys("1234567");
         webDriver.findElement(By.xpath("//input[@class='ant-input ant-select-search__field']")).sendKeys("www.hoa.com");
@@ -102,9 +103,9 @@ public class TestComponent extends Master{
         webDriver.findElement(By.xpath("//button[contains(.,'Register')]")).click();
     }
 
-//ant - InputNumber
+    //ant - InputNumber
     @Test
-    public void testInputNumber(){
+    public void testInputNumber() {
         webDriver.get("https://ant.design/components/input-number/");
         WebElement Element = webDriver.findElement(By.xpath("//input[@value='$ 1,000']"));
         JavascriptExecutor js = (JavascriptExecutor) webDriver;
@@ -115,35 +116,35 @@ public class TestComponent extends Master{
         inputNumber.sendKeys("$ 2,000");
     }
 
-//ant - Transfer
+    //ant - Transfer
     @Test
-    public void testTransfer1(){
+    public void testTransfer1() {
         webDriver.get("https://ant.design/components/transfer/");
         transfer("//section[@id='components-transfer-demo-basic']//div[@class='ant-transfer-list-body']//div[@class='LazyLoad is-visible']//li//span", "content2");
     }
 
     @Test
-    public void testTransfer2(){
+    public void testTransfer2() {
         webDriver.get("https://ant.design/components/transfer/");
 
         String s1 = "content2;content5";
         String[] temp = s1.split(";");
-        for(String w:temp){
-            String optionTrans = String.format("//section[@id='components-transfer-demo-basic']//span[text()='%s']",w);
+        for (String w : temp) {
+            String optionTrans = String.format("//section[@id='components-transfer-demo-basic']//span[text()='%s']", w);
             WebElement optionXpath = webDriver.findElement(By.xpath(optionTrans));
             optionXpath.click();
         }
     }
 
-//ant - Timepicker
+    //ant - Timepicker
     @Test
-    public void testTimepicker(){
+    public void testTimepicker() {
         webDriver.get("https://ant.design/components/time-picker/");
         webDriver.findElement(By.xpath("//section[@id='components-time-picker-demo-basic']//input[@class='ant-time-picker-input']")).click();
         webDriver.findElement(By.xpath("//section[@id='components-time-picker-demo-basic']//input[@class='ant-time-picker-input']")).sendKeys("02:05:20");
     }
 
-//ant - Dropdown
+    //ant - Dropdown
     @Test
     public void testDropdown() throws InterruptedException {
         webDriver.get("https://ant.design/components/dropdown/");
@@ -156,67 +157,70 @@ public class TestComponent extends Master{
         webDriver.findElement(By.xpath("//li[contains(.,'1st menu item')]")).click();
     }
 
-//ant - Checkbox
+    //ant - Checkbox
     @Test
-    public void testCheckbox1(){
+    public void testCheckbox1() {
         webDriver.get("https://ant.design/components/checkbox/");
         webDriver.findElement(By.xpath("//section[@id='components-checkbox-demo-basic']//input[@type='checkbox']")).click();
     }
 
     @Test
-    public void testCheckbox2(){
+    public void testCheckbox2() {
         webDriver.get("https://ant.design/components/checkbox/");
         webDriver.findElement(By.xpath("//section[@id='components-checkbox-demo-controller']//input[@type='checkbox']")).click();
     }
+
     @Test
-    public void testCheckbox3(){
+    public void testCheckbox3() {
         webDriver.get("https://ant.design/components/checkbox/");
         webDriver.findElement(By.xpath("//button[contains(.,'Uncheck')]")).click();
     }
+
     @Test
-    public void testCheckbox4(){
+    public void testCheckbox4() {
         webDriver.get("https://ant.design/components/checkbox/");
         webDriver.findElement(By.xpath("//button[contains(.,'Disable')]")).click();
     }
 
     @Test
-    public void testCheckbox5(){
+    public void testCheckbox5() {
         webDriver.get("https://ant.design/components/checkbox/");
         scrollToElement("//section[@id='components-checkbox-demo-check-all']//span[contains(.,'Check all')]");
         webDriver.findElement(By.xpath("//section[@id='components-checkbox-demo-check-all']//div[@class='ant-checkbox-group']//span[contains(.,'Apple')]")).click();
         webDriver.findElement(By.xpath("//section[@id='components-checkbox-demo-check-all']//div[@class='ant-checkbox-group']//span[contains(.,'Pear')]")).click();
     }
 
-//ant - Mentions
+    //ant - Mentions
     @Test
-    public void testMention(){
+    public void testMention() {
         webDriver.get("https://ant.design/components/mentions/");
         webDriver.findElement(By.xpath("//section[@id='components-mentions-demo-basic']//textarea")).sendKeys("@");
     }
 
-//ant - Rate
+    //ant - Rate
     @Test
-    public void testRate(){
+    public void testRate() {
         webDriver.get("https://ant.design/components/rate/");
 
     }
 
-//ant - Radio
+    //ant - Radio
     @Test
-    public void testRadio1(){
+    public void testRadio1() {
         webDriver.get("https://ant.design/components/radio/");
         webDriver.findElement(By.xpath("//section[@id='components-radio-demo-radiogroup']//section[@class='code-box-demo']//span[contains(.,'A')]")).click();
     }
+
     @Test
-    public void testRadio2(){
+    public void testRadio2() {
         webDriver.get("https://ant.design/components/radio/");
         webDriver.findElement(By.xpath("//section[@id='components-radio-demo-radiogroup']//section[@class='code-box-demo']//span[contains(.,'A')]")).click();
         webDriver.findElement(By.xpath("//section[@id='components-radio-demo-radiogroup']//section[@class='code-box-demo']//span[contains(.,'B')]")).click();
     }
 
-//ant - Switch
+    //ant - Switch
     @Test
-    public void testSwitch(){
+    public void testSwitch() {
         webDriver.get("https://ant.design/components/switch/");
         webDriver.findElement(By.xpath("//section[@id='components-switch-demo-basic']//button[@type='button']")).click();
         webDriver.findElement(By.xpath("//section[@id='components-switch-demo-basic']//button[@type='button']")).click();
